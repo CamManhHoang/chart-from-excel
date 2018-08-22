5ae941f817f75adefa57bfd3aadec02d69420e71
camhm <cam.hoang@eastgate-software.com>
2018-08-22 13:38:10 +0700

    n+2 commit

--- index.js
+++ index.js
@@ -1,3 +1,55 @@
+/* CREATE SKELETON CHART JS */
+let ctx = document.getElementById('my-chart');
+let bubbleData = {
+  datasets: [],
+};
+let options = {
+  scales: {
+    yAxes: [
+      {
+        scaleLabel: {
+          display: true,
+          labelString: 'Yield',
+        },
+        ticks: {
+          max: 6,
+          min: 2.8,
+          stepSize: 0.2,
+        },
+      }],
+    xAxes: [
+      {
+        scaleLabel: {
+          display: true,
+          labelString: 'Year To Mature',
+        },
+        ticks: {
+          max: 8,
+          min: 0,
+          stepSize: 1,
+        },
+      }],
+  },
+  tooltips: {
+    callbacks: {
+      label: function(tooltipItem, data) {
+        var label = data.labels[tooltipItem.index];
+        return label + ': (' + tooltipItem.xLabel + ', ' +
+            tooltipItem.yLabel + ')';
+      },
+    },
+  },
+  legend: {
+    display: false,
+  },
+};
+const myChart = new Chart(ctx, {
+  type: 'bubble',
+  data: bubbleData,
+  options: options,
+});
+
+/* UPLOAD EXCEL FILE AND HANDLE IT */
 var XLSX = require('xlsx');
 var rABS = false; // true: readAsBinaryString ; false: readAsArrayBuffer
 
@@ -35,58 +87,15 @@ function handleFile(e) {
       });
     });
 
-    let ctx = document.getElementById('my-chart');
-    let bubbleData = {
-      labels: names,
-      datasets: [
-        {
-          label: 'Data',
-          data: positions,
-          backgroundColor: '#03DAC6',
-        }],
+    /* Update Chart with new data from excel */
+    const newDataset = {
+      label: 'Data',
+      data: positions,
+      backgroundColor: '#03DAC6',
     };
-    let options = {
-      scales: {
-        yAxes: [
-          {
-            scaleLabel: {
-              display: true,
-              labelString: 'Yield',
-            },
-            ticks: {
-              max: 6,
-              min: 2.8,
-              stepSize: 0.2,
-            },
-          }],
-        xAxes: [
-          {
-            scaleLabel: {
-              display: true,
-              labelString: 'Year To Mature',
-            },
-            ticks: {
-              max: 8,
-              min: 0,
-              stepSize: 1,
-            },
-          }],
-      },
-      tooltips: {
-        callbacks: {
-          label: function(tooltipItem, data) {
-            var label = data.labels[tooltipItem.index];
-            return label + ': (' + tooltipItem.xLabel + ', ' +
-                tooltipItem.yLabel + ')';
-          },
-        },
-      },
-    };
-    new Chart(ctx, {
-      type: 'bubble',
-      data: bubbleData,
-      options: options,
-    });
+    bubbleData.datasets.push(newDataset);
+    bubbleData.labels = names;
+    myChart.update();
   };
   if (rABS) reader.readAsBinaryString(f); else reader.readAsArrayBuffer(f);
 }
